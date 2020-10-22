68d47ff0d325fa4d6598358564783ba05f9903a2
Aries <aho11@stuy.edu>
2020-10-22 18:23:03 -0400

    created the methods and implemented first sum

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,50 @@
-Public class ArrayOps{
-  
+public class ArrayOps{
+  public static int sum(int[] arr){
+    int total = 0;
+    for(int i = 0; i < arr.length; i++){
+      total = total + arr[i];
+    }
+    return total;
+  }
+
+  public static int largest(int[] arr){
+    int large = 0;
+    return large;
+  }
+
+  public static int[] sumRows(int[][] matrix){
+    int[] sum = new int[matrix.length];
+    return sum;
+  }
+
+  public static int[] largestInRows(int[][] matrix){
+    int[] largest = new int[matrix.length];
+    return largest;
+  }
+
+  public static int sum(int[][] arr){
+    int total = 0;
+    return total;
+  }
+
+  public static int[] sumCols(int[][] matrix){
+    int[] sum = new int[matrix.length];
+    return sum;
+  }
+
+  public static boolean isRowMagic(int[][] matrix){
+    boolean samesum = false;
+    return samesum;
+  }
+
+  public static boolean isColMagic(int[][] matrix){
+    boolean samesum = false;
+    return samesum;
+  }
+
+  public static boolean isLocationMagic(int[][] matrix, int row, int clmn){
+    boolean samesum = false;
+    return samesum;
+  }
+
 }
