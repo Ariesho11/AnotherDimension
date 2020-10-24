993383b4d689f1ae85ceeb90226fd7d8a61eb7e1
Aries <aho11@stuy.edu>
2020-10-23 20:05:30 -0400

    implemented and tested the first largest method

--- ArrayOps.java
+++ ArrayOps.java
@@ -9,6 +9,9 @@ public class ArrayOps{
 
   public static int largest(int[] arr){
     int large = 0;
+    for(int i = 0 ; i < arr.length; i++){
+      if (arr[i] > large) large = arr[i];
+    }
     return large;
   }
 
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+  public static void main(String[] args){
+    int[] testme = {1, 4, 2, 3};
+    System.out.println(ArrayOps.sum(testme));
+    System.out.println(ArrayOps.largest(testme));
+  }
+}
